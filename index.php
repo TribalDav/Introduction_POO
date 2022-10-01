0c0008b60facf553ee532b295dd472e87d65d44d
TribalDav <tribaldav@gmail.com>
2022-10-01 15:57:50 +0200

    reste à résoudre le problème d'enregistrement de persos, ajout également d'un message utilisateur connecté

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
 
 require 'model/entities/Character.php';
-require 'model/entities/Wizard.php';
+// require 'model/entities/Wizard.php';
 require 'model/manager/CharacterManager.php';
-/*
-$harry = new Character("Harry", 200, 30);
+
+
+                                            /*   Test de la classe Character début de l'exercice  */
+
+/*$harry = new Character("Harry", 200, 30);
 var_dump($harry);
 $drago = new Character("Drago", 100, 15);
 var_dump($drago);
@@ -15,6 +18,8 @@ echo $harry;
 $hermione = new Wizard("Hermione", 175, 50);
 var_dump($hermione);
 */
+
+
 $characters = CharacterManager::getAllCharacters();
 // var_dump($characters);
 
